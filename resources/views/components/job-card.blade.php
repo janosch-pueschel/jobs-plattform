6bd90c959a2d34306dc9566196821308c98cf354
Janosch Püschel <janosch.pueschel@gmail.com>
2024-07-28 15:46:12 +0200

    feat: add job card component

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,16 @@
+<div class="flex flex-col rounded-xl bg-white/5 p-4 text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+    <div class="mt-auto flex items-center justify-between">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img class="rounded" src="http://placehold.it/42/42" alt="job-logo">
+    </div>
+</div>
