0e265e4fe9e28aabd8272e496e6f1e82746024d2
Janosch Püschel <janosch.pueschel@gmail.com>
2024-07-29 12:37:49 +0200

    refactor: tag component style variants

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,2 +1,13 @@
-<a class="rounded-xl bg-white/10 px-3 py-1 text-2xs font-bold transition-colors duration-300 hover:bg-white/25"
-    href="#">{{ $slot }}</a>
+@props(['size' => 'base'])
+
+@php
+    $defaultStyle = 'rounded-xl bg-white/10 font-bold transition-colors duration-300 hover:bg-white/25';
+
+    if ($size === 'base') {
+        $defaultStyle .= ' px-5 py-1 text-sm';
+    } elseif ($size === 'small') {
+        $defaultStyle .= ' px-3 py-1 text-2xs';
+    }
+@endphp
+
+<a class="{{ $defaultStyle }}" href="#">{{ $slot }}</a>
