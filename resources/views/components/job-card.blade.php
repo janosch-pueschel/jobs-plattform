4fe0fc292fc33128930ade7d82749ca02f3a8694
Janosch Püschel <janosch.pueschel@gmail.com>
2024-07-29 12:19:20 +0200

    refactor: job card styling

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,8 +1,9 @@
-<div class="flex flex-col rounded-xl bg-white/5 p-4 text-center">
+<div
+    class="group flex flex-col rounded-xl border border-transparent bg-white/5 p-4 text-center transition-colors duration-300 hover:border-blue-400">
     <div class="self-start text-sm">Laracasts</div>
-    <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="text-xl font-bold transition-colors duration-400 group-hover:text-blue-600">Video Producer</h3>
+        <p class="mt-4 text-sm">Full Time - From $60,000</p>
     </div>
     <div class="mt-auto flex items-center justify-between">
         <div>
@@ -11,6 +12,6 @@
             <x-tag>Tag</x-tag>
         </div>
 
-        <img class="rounded" src="http://placehold.it/42/42" alt="job-logo">
+        <x-employer-logo :width="42" />
     </div>
 </div>
