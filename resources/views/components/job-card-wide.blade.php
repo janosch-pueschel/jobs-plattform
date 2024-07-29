4230774ff994e42829faf1a855dbe8e82f49825d
Janosch Püschel <janosch.pueschel@gmail.com>
2024-07-29 12:19:40 +0200

    feat: add job card wide component

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,15 @@
+<div class="flex gap-x-6 rounded-xl border border-transparent bg-white/5 p-4 hover:border-blue-600 group transition-colors duration-400">
+    <div>
+        <x-employer-logo />
+    </div>
+    <div class="flex flex-1 flex-col">
+        <a class="self-start text-sm text-gray-400" href="#">Laracasts</a>
+        <h3 class="mt-3 text-xl font-bold group-hover:text-blue-600 transition-colors duration-400">Video Producer</h3>
+        <p class="mt-auto text-sm text-gray-400">Full Time - From $60,000</p>
+    </div>
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
