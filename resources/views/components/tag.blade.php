96161a90bd4772681f06b5a517889a0192b2471d
Janosch Püschel <janosch.pueschel@gmail.com>
2024-07-29 12:19:08 +0200

    refactor: tag component styling

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,2 +1,2 @@
-<a class="rounded-xl bg-white/10 px-2 py-1 text-xs transition-colors duration-300 hover:bg-white/25"
+<a class="rounded-xl bg-white/10 px-3 py-1 text-2xs font-bold transition-colors duration-300 hover:bg-white/25"
     href="#">{{ $slot }}</a>
