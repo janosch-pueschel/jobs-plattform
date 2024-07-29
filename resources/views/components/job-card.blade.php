221943ab2a7211280953baac1c684bcc72bce469
Janosch Püschel <janosch.pueschel@gmail.com>
2024-07-29 12:29:49 +0200

    refactor: add panel component to job card wide component

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,8 +1,7 @@
-<div
-    class="group flex flex-col rounded-xl border border-transparent bg-white/5 p-4 text-center transition-colors duration-300 hover:border-blue-400">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">Laracasts</div>
     <div class="py-8">
-        <h3 class="text-xl font-bold transition-colors duration-400 group-hover:text-blue-600">Video Producer</h3>
+        <h3 class="duration-400 text-xl font-bold transition-colors group-hover:text-blue-600">Video Producer</h3>
         <p class="mt-4 text-sm">Full Time - From $60,000</p>
     </div>
     <div class="mt-auto flex items-center justify-between">
@@ -14,4 +13,4 @@
 
         <x-employer-logo :width="42" />
     </div>
-</div>
+</x-panel>
