166adefe61fac5447519d3f05d603914ee5f567b
Janosch Püschel <janosch.pueschel@gmail.com>
2024-07-30 22:52:23 +0200

    refactor: add necessary prop to employer logo

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -2,7 +2,7 @@
 
 <x-panel class="flex gap-x-6">
     <div>
-        <x-employer-logo />
+        <x-employer-logo :employer="$job->employer" />
     </div>
     <div class="flex flex-1 flex-col">
         <a class="self-start text-sm text-gray-400" href="#">{{ $job->employer->name }}</a>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -15,6 +15,6 @@
             @endforeach
         </div>
 
-        <x-employer-logo :width="42" />
+        <x-employer-logo :width="42" :employer="$job->employer" />
     </div>
 </x-panel>
