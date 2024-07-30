d30bb1182b03a7a85167a29883291ece95c5de19
Janosch Püschel <janosch.pueschel@gmail.com>
2024-07-30 19:06:36 +0200

    refactor: use data from database

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -5,13 +5,14 @@
         <x-employer-logo />
     </div>
     <div class="flex flex-1 flex-col">
-        <a class="self-start text-sm text-gray-400" href="#">Laracasts</a>
-        <h3 class="duration-400 mt-3 text-xl font-bold transition-colors group-hover:text-blue-600">Video Producer</h3>
-        <p class="mt-auto text-sm text-gray-400">Full Time - From $60,000</p>
+        <a class="self-start text-sm text-gray-400" href="#">{{ $job->employer->name }}</a>
+        <h3 class="duration-400 mt-3 text-xl font-bold transition-colors group-hover:text-blue-600">{{ $job->title }}
+        </h3>
+        <p class="mt-auto text-sm text-gray-400">{{ $job->schedule }} - {{ $job->salary }}</p>
     </div>
     <div>
         @foreach ($job->tags as $tag)
-            <x-tag :tag={{ $tag }} size="small" />
+            <x-tag :tag="$tag" size="small" />
         @endforeach
     </div>
 </x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,15 +1,15 @@
 @props(['job'])
 
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
     <div class="py-8">
-        <h3 class="duration-400 text-xl font-bold transition-colors group-hover:text-blue-600">Video Producer</h3>
-        <p class="mt-4 text-sm">Full Time - From $60,000</p>
+        <h3 class="duration-400 text-xl font-bold transition-colors group-hover:text-blue-600">{{ $job->title }}</h3>
+        <p class="mt-4 text-sm">{{ $job->schedule }} - From {{ $job->salary }}</p>
     </div>
     <div class="mt-auto flex items-center justify-between">
         <div>
             @foreach ($job->tags as $tag)
-                <x-tag :tag={{ $tag }} size="small" />
+                <x-tag :tag="$tag" size="small" />
             @endforeach
         </div>
 
